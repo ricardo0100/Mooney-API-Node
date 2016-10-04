4325ea8078bf2ae133e8590c1e71ae9603419980
Ricardo Gehrke Filho <ricardo.gehrke@nkey.com.br>
2016-10-04 09:10:53 -0300

    Fix post array of accounts

--- model/account.js
+++ model/account.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
 
 var accountSchema = mongoose.Schema({
-  _id: { type: String },
-  name: { type: String },
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
   removed: { type: Boolean, default: false }
-}, { useNestedStrict: true, timestamps: true });
+}, { useNestedStrict: true, timestamps: { createdAt: 'created_at', updatedAt:  'updated_at' } });
+
+accountSchema.pre('findOneAndUpdate', function(next) {
+  
+  next();
+});
 
 var model = mongoose.model('Account', accountSchema);
 
--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var async = require('async')
 var Account = require('../../model/account');
 
 router.get('/accounts', function(request, response, next) {
-  Account.find({}).exec()
+  Account.find({ removed: false }).exec()
   .then(function(accounts) {
     response.json(accounts);
   })
@@ -14,18 +14,51 @@ router.get('/accounts', function(request, response, next) {
 });
 
 router.post('/accounts', function(request, response, next) {
-  for (var index in request.body) {
-    var account = request.body[index];
-    Account.update({ uuid: account.uuid }, account, { upsert: true }).exec()
-    .then(function(raw) {
-      response.status(200);
-      response.send(raw);
-    })
-    .then(function(error) {
+  var inserts = [];
+  var updates = [];
+  var errors = [];
+  
+  async.each(request.body, function iteratee(account, callback) {
+
+    var newAccount = new Account(account);
+    var validationError = newAccount.validateSync();
+    if(validationError) {
+      errors.push({ key: account._id, value: validationError.errors });
+      callback();
+    } else {
+      Account.findOneAndUpdate({ _id: newAccount._id }, newAccount, { upsert: true })
+      .then(function(document) {
+        if(document) {
+          updates.push(account._id);
+        } else {
+          inserts.push(account._id);
+        }
+      })
+      .then(function(err) {
+        if(err) {
+          errors.push({ key: account._id, value: err });
+        }
+        callback(err);
+      });
+    }
+    
+  }, function(err) {
+
+    if(err) {
       response.status(400);
-      next(error);
-    });
-  }
+      response.send(err);
+      next(err);
+    } else {
+      response.status(200);
+      response.send({
+        inserted: inserts,
+        updated: updates,
+        errors: errors
+      });
+      next();
+    }
+
+  });
 });
 
 module.exports = router;
